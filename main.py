26d1453f0f41c87dad4bbfa9edc248e881a8cab2
mbalon <balonmateusz@gmail.com>
2021-02-21 18:33:33 +0100

    initial commit, add fun get_page_size and convert_size_to_mm

---
+++ main.py
@@ -0,0 +1,20 @@
+from PyPDF2 import PdfFileReader
+from reportlab.lib.units import mm
+from typing import List
+
+path_name = r'C:\Users\balon\Desktop\kartkaSosin1.pdf'
+
+
+def get_page_size(path):
+    with open(path, 'rb') as file:
+        pdf = PdfFileReader(file)
+        page = pdf.getPage(0)
+        return page["/MediaBox"][2:4]
+
+
+def convert_size_to_mm(pt_size: List):
+    return [float(size)/mm for size in pt_size]
+
+
+page_size = get_page_size(path_name)
+print(convert_size_to_mm(page_size))
